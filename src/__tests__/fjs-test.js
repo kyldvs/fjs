e72f31309b47a40289ddc0e6910cdd45bc3141ec
Kyle Davis <kad@fb.com>
2016-06-23 15:13:26 -0700

    Makes jest work

---
+++ src/__tests__/fjs-test.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ */
+
+import fjs from '../fjs/';
+
+it('lives!!', () => {
+  expect(fjs()).toBe(1);
+});
---
+++ src/fjs.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * @flow
+ */
+
+'use strict';
+
+function fjs() {
+  return 1;
+}
+
+export default fjs;
