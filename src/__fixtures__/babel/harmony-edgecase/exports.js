ead874b3b121102dee894f59bef2f686d7a42c4c
Kyle Davis <kad@fb.com>
2016-07-08 15:40:55 -0700

    Add tests from babel-generator

---
+++ src/__fixtures__/babel/harmony-edgecase/exports.js
@@ -0,0 +1,20 @@
+export * from "OK"
+export { name } from "OK"
+export { a as b, c as d } from "hello"
+export { a as b, c as d }
+export { }
+export default i = 20
+export function test() { }
+export class test2 { }
+export var i = 20
+export let i = 42
+
+export default (function(){})();
+export default (class {})();
+export default {};
+
+export default function test3(){}
+export default class test4 {}
+
+export default function(){}
+export default class {}
